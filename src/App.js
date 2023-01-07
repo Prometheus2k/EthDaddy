0f0f31f3453134c0d4779b8046c79c7e8426da92
Prometheus2k <shonesk2000@gmail.com>
2023-01-07 21:05:42 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -14,13 +14,7 @@ import config from "./config.json";
 
 function App() {
   const [account, setAccount] = useState(null);
-  const loadBlockchainData = async () => {
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    const account = ethers.utils.getAddress(accounts[0]);
-    setAccount(account);
-  };
+  const loadBlockchainData = async () => {};
 
   useEffect(() => {
     loadBlockchainData();
@@ -28,9 +22,9 @@ function App() {
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
       <div className="cards__section">
         <h2 className="cards__title">Welcome to ETH Daddy!</h2>
-        <p>{account}</p>
       </div>
     </div>
   );
