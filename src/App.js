7d7f85df0b87ac6d34b28e6b6537a6a7abebe70f
Prometheus2k <shonesk2000@gmail.com>
2023-01-08 09:24:27 +0530

    Listed domains in website

--- src/App.js
+++ src/App.js
@@ -13,8 +13,37 @@ import ETHDaddy from "./abis/ETHDaddy.json";
 import config from "./config.json";
 
 function App() {
+  const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
+
+  const [ethDaddy, setEthDaddy] = useState(null);
+  const [domains, setDomains] = useState([]);
+
   const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+    console.log(network);
+
+    const ethDaddy = new ethers.Contract(
+      config[network.chainId].ETHDaddy.address,
+      ETHDaddy,
+      provider,
+    );
+
+    setEthDaddy(ethDaddy);
+
+    const maxSupply = await ethDaddy.maxSupply();
+    const domains = [];
+
+    for (let i = 1; i <= maxSupply; i++) {
+      const domain = await ethDaddy.getDomain(i);
+      domains.push(domain);
+    }
+    console.log(domains);
+    setDomains(domains);
+
     window.ethereum.on("accountsChanged", async () => {
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
@@ -41,7 +70,17 @@ function App() {
         </p>
         <hr />
 
-        <div className="cards"></div>
+        <div className="cards">
+          {domains.map((domain, index) => (
+            <Domain
+              domain={domain}
+              ethDaddy={ethDaddy}
+              provider={provider}
+              id={index + 1}
+              key={index}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Domain.js
+++ src/components/Domain.js
@@ -1,13 +1,26 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 const Domain = ({ domain, ethDaddy, provider, id }) => {
-
   return (
-    <div className='card'>
+    <div className="card">
+      <div className="card__info">
+        <h3>{domain.name}</h3>
+        <p>
+          <>
+            <strong>
+              {ethers.utils.formatUnits(domain.cost.toString(), "ether")}
+            </strong>
+            ETH
+          </>
+        </p>
+      </div>
 
+      <button type="button" className="card__button">
+        Buy it
+      </button>
     </div>
   );
-}
+};
 
 export default Domain;
